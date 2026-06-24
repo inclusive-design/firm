98d07f00e19d0f98006432a3b50f9f3606b4e426
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-06-24 16:20:31 -0300

    fix: improve eleventy.before hook, add docs

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ to export variables from Figma.
 
 We use the following advanced settings:
 
-- [x] Split collections into separate files
-- [x] Split modes into separate files
-- [x] Omit collection names
+- [ ] Split collections into separate files
+- [ ] Split modes into separate files
+- [ ] Omit collection names
 - [ ] Include variable scopes
 - [ ] Use percentage for opacity
 - [x] USe DTCG keys format
@@ -18,19 +18,20 @@ We use the following advanced settings:
 
 To obtain the Design Token JSON files from Tokens Brücke, open the plugin, confirm
 that the settings are configured correctly, and then click **Download JSON** to
-download a zip file containing the tokens.
+download the token file. With a GitHub token, it can also be synced directly to
+the repo: [src/design-tokens/synced/design.tokens.json](src/design-tokens/synced/design.tokens.json)
 
-Some adjustments need to be made to the downloaded files, as follows:
+Some adjustments need to be made to the downloaded file, as follows:
 
-1.
+1. Colors need to be converted from hex strings to OKLCH.
+2. A generic UIO theme needs to be created which maps UIO primitves to different
+   color tokens.
+3. Dark mode and contrast themes need to be extracted into their own token files.
+4. Unnecessary group prefixes need to be removed from variable names.
+5. Font weight, leading and dimension values need to be properly formatted to
+   match the DTCG spec.
+6. Extraneous properties need to be removed.
+7. The `$type` values need to be standardized to match the DTCG spec.
+8. The updated tokens need to be written to their own files in the repo.
 
-2. Border widths need to be converted to the appropriate format:
-
-```diff
-        "$type": "dimension",
--       "$value": "1px",
-+       "$value": {
-+         "value": 1,
-+         "unit": "px"
-+       }
-```
+These steps are all handled in an [`eleventy.before` hook](https://github.com/inclusive-design/firm/blob/c2c8d77c11814ad18d8d19845a9904657bfe5e06/eleventy.config.js#L36).
--- eleventy.config.js
+++ eleventy.config.js
@@ -37,8 +37,8 @@ export default function eleventy(eleventyConfig) {
 		const { palette, aliases, colors, borders, typography } = synced;
 
 		/**
-		 * Recursively convert HEX colors to DTCG-compliant OKLCH colors in synced design tokens.
-		 * @param {object} object - The synced design tokens object.
+		 * Recursively convert hex string colors to DTCG-compliant OKLCH colors.
+		 * @param {object} object - A DTCG object containing hex string colors.
 		 */
 		const processColors = (object) => {
 			for (const value of Object.values(object)) {
@@ -69,6 +69,12 @@ export default function eleventy(eleventyConfig) {
 			}
 		};
 
+		/**
+		 * Recursively replace theme colors with value from Cobalt's legacy mode format.
+		 * @see https://cobalt-ui.pages.dev/guides/modes#with-modes
+		 * @param {object} colorObject - A DTCG object containing colors with themes in Cobalt's legacy mode format.
+		 * @param {string} mode - The mode key whose value should be used.
+		 */
 		const processThemes = (colorObject, mode) => {
 			for (const value of Object.values(colorObject)) {
 				if (typeof value === 'object') {
@@ -82,30 +88,42 @@ export default function eleventy(eleventyConfig) {
 		};
 
 		/**
-		 * Remove prefixes from value aliases.
-		 * @param {object} object - The synced design tokens object.
-		 * @param {string} prefix - The prefix to remove.
+		 * Recursively remove the given prefixes from value aliases.
+		 * @param {object} object - A DTCG object containing prefixed value aliases.
+		 * @param {Array} prefixes - The prefixes to remove.
 		 */
-		const removePrefixes = (object, prefix) => {
+		const removePrefixes = (object, prefixes) => {
 			for (const value of Object.values(object)) {
 				if (typeof value === 'object') {
-					if (Object.hasOwn(value, '$value') && typeof value.$value === 'string' && value.$value.startsWith(`{${prefix}.`)) {
-						value.$value = value.$value.replace(`{${prefix}.`, '{');
+					if (Object.hasOwn(value, '$value') && typeof value.$value === 'string') {
+						for (const prefix of prefixes) {
+							value.$value = value.$value.startsWith(`{${prefix}.`)
+								? value.$value.replace(`{${prefix}.`, '{')
+								: value.$value;
+						}
 					} else {
-						removePrefixes(value, prefix);
+						removePrefixes(value, prefixes);
 					}
 				}
 			}
 		};
 
+		/**
+		 * Recursively convert numeric font weights stored as strings into integers.
+		 * @param {object} weights - A DTCG object containing font weights stored as strings.
+		 */
 		const processWeights = (weights) => {
 			for (const weight of Object.keys(weights)) {
-				if (weight !== '$type') {
+				if (weight !== '$type' && typeof weights[weight].$value === 'string') {
 					weights[weight].$value = Number.parseInt(weights[weight].$value, 10);
 				}
 			}
 		};
 
+		/**
+		 * Recursively convert text leadings stored as strings into floats, removing unnecessary `px` suffixes.
+		 * @param {object} leadings - A DTCG object containing text leadings stored as strings.
+		 */
 		const processLeadings = (leadings) => {
 			for (const leading of Object.keys(leadings)) {
 				if (typeof leadings[leading].$value === 'string') {
@@ -114,49 +132,64 @@ export default function eleventy(eleventyConfig) {
 			}
 		};
 
-		const processBorders = (bordersObject) => {
-			for (const border of Object.keys(bordersObject)) {
-				if (typeof bordersObject[border].$value === 'string') {
-					bordersObject[border].$value = { value: Number.parseInt(bordersObject[border].$value.replace('px', ''), 10), unit: 'px' };
+		/**
+		 * Recursively convert dimensions with a unit stored as a string into a DTCG-compliant value object.
+		 * @param {object} dimensions - A DTCG object containing dimensions stored as strings.
+		 */
+		const processDimensions = (dimensions) => {
+			for (const dimension of Object.keys(dimensions)) {
+				if (typeof dimensions[dimension].$value === 'string') {
+					dimensions[dimension].$value = { value: Number.parseInt(dimensions[dimension].$value.replace('px', ''), 10), unit: 'px' };
 				}
 			}
 		};
 
 		/**
-		 * Remove extra properties from synced design tokens.
-		 * @param {object} object The object from where you want to remove the keys
-		 * @param {string} key The property name to remove.
+		 * Remove extra properties from design tokens.
+		 * @param {object} object - A DTCG object containing extraneous properties.
+		 * @param {Array} properties - The properties to remove.
 		 */
-		const removeExtraProperties = (object, key) => {
+		const removeExtraProperties = (object, properties) => {
 			for (const prop in object) {
 				if (Object.hasOwn(object, prop)) {
-					if (typeof (object[prop]) === 'string' && key === prop) {
+					if (typeof (object[prop]) === 'string' && properties.includes(prop)) {
 						delete object[prop];
 					}
 
 					if (typeof (object[prop]) === 'object') {
-						if (key === prop) {
+						if (properties.includes(prop)) {
 							delete object[prop];
 						} else {
-							removeExtraProperties(object[prop], key);
+							removeExtraProperties(object[prop], properties);
 						}
 					}
 				}
 			}
 		};
 
+		/**
+		 * 1. Convert colors from hex to OKLCH.
+		 */
 		processColors(palette);
 
+		/**
+		 * 2. Create the generic UIO theme which assigns UIO primitives to all the required colors.
+		 */
 		const themes = { uio: structuredClone(colors) };
 
 		delete themes.uio.color.uio;
 
+		// All the UIO themes assign UIO primitives the same way, so we can select any to remap.
 		processThemes(themes.uio, 'bw');
 
+		/**
+		 * 3. Create all of the other contrast themes.
+		 */
 		for (const theme of ['dark', 'bw', 'wb', 'by', 'yb', 'gd', 'gw', 'lgdg', 'bbr']) {
 			themes[theme] = structuredClone(colors);
 			themes[theme].color.$type = 'color';
 
+			// Remove duplicate properties from UIO theme as they're already assigned in themes.uio (see step 2).
 			if (theme !== 'dark') {
 				for (const key of Object.keys(themes[theme].color)) {
 					if (key !== 'uio') {
@@ -165,41 +198,39 @@ export default function eleventy(eleventyConfig) {
 				}
 			}
 
-			console.log(themes[theme].color);
-
 			processThemes(themes[theme], theme);
 		}
 
-		removePrefixes(aliases, 'palette');
-		removePrefixes(colors, 'palette');
-		removePrefixes(colors, 'aliases');
-		removePrefixes(colors, 'colors');
-		removePrefixes(themes, 'palette');
-		removePrefixes(themes, 'aliases');
-		removePrefixes(themes, 'colors');
-		removePrefixes(typography, 'typography');
-		removePrefixes(typography, 'responsive');
+		/**
+		 * 4. Remove all the unnecessary group prefixes.
+		 */
+		removePrefixes(aliases, ['palette']);
+		removePrefixes(colors, ['palette', 'aliases', 'colors']);
+		removePrefixes(themes, ['palette', 'aliases', 'colors']);
+		removePrefixes(typography, ['typography', 'responsive']);
 
+		/**
+		 * 5. Clean up values which aren't exported cleanly from Figma: font weight, leading, dimensions.
+		 */
 		processWeights(typography.font.weight);
 
 		processLeadings(typography.leading);
 
-		processBorders(borders.border.width);
+		processDimensions(borders.border.width);
 
+		/**
+		 * 6. Remove extraneous properties.
+		 */
 		for (const object of [palette, aliases, colors, borders, typography]) {
-			removeExtraProperties(object, '$extensions');
-			removeExtraProperties(object, '$description');
-			removeExtraProperties(object, '$type');
+			removeExtraProperties(object, ['$extensions', '$description', '$type']);
 		}
 
 		for (const theme of Object.keys(themes)) {
-			removeExtraProperties(themes[theme], '$extensions');
-			removeExtraProperties(themes[theme], '$description');
-			removeExtraProperties(themes[theme], '$type');
+			removeExtraProperties(themes[theme], ['$extensions', '$description', '$type']);
 		}
 
 		/**
-		 * 3. Standardize types to match DTCG specification.
+		 * 7. Standardize types to match DTCG specification.
 		 */
 		palette.$type = 'color';
 		aliases.$type = 'color';
@@ -213,7 +244,7 @@ export default function eleventy(eleventyConfig) {
 		typography.diagram.$type = 'dimension';
 		typography.leading.$type = 'number';
 
-		/** 4. Write to typography.json and palette.json. */
+		/** 8. Write to typography.json and palette.json. */
 
 		for (const [key, value] of Object.entries({
 			palette, aliases, colors, borders, typography,
