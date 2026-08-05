304accc144319100d4e515169497c01c10d2910d
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-08-05 15:40:18 -0300

    fix: remove components (#16)

--- eslint.config.js
+++ eslint.config.js
@@ -4,9 +4,6 @@ import eslintConfigInclusiveDesign from '@inclusive-design/eslint-config';
 export default defineConfig([
 	{
 		extends: [eslintConfigInclusiveDesign],
-		rules: {
-			'new-cap': ['error', { capIsNewExceptions: ['Elena'] }],
-		},
 	},
 	{
 		ignores: ['_site/**', 'README.md', '**/package-lock.json'],
