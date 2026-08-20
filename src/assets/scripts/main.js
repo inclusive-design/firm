87b5fc31a17d1b5cdf610abc31eee88865e7b0df
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-08-20 10:42:52 -0300

    feat: basic navigation layout and states

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,1 +1,9 @@
 import 'virtual:sugarcube.css';
+
+const disclosureButtons = document.querySelectorAll('[aria-expanded]');
+for (const button of disclosureButtons) {
+	button.addEventListener('click', (_event) => {
+		const expanded = button.getAttribute('aria-expanded') === 'true' || false;
+		button.setAttribute('aria-expanded', !expanded);
+	});
+}
