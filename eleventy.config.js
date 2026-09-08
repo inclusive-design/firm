b64b3afab2edcf7e109796d596395d3488acaa03
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-09-08 10:47:53 -0300

    fix: resolve lint issue

--- eleventy.config.js
+++ eleventy.config.js
@@ -31,170 +31,176 @@ export default function eleventy(eleventyConfig) {
 
 	eleventyConfig.addPassthroughCopy('src/assets');
 
-	let firstRun = true;
+	let isFirstRun = true;
 
 	eleventyConfig.on('eleventy.before', async ({ _directories, _runMode, _outputMode }) => {
-		if (firstRun) {
-			firstRun = false;
+		if (!isFirstRun) {
+			return;
+		}
 
-			const { palette, aliases, colors, borders, space, typography } = synced;
+		isFirstRun = false;
 
-			/**
-			 * Recursively replace theme colors with value from Cobalt's legacy mode format.
-			 * @see https://cobalt-ui.pages.dev/guides/modes#with-modes
-			 * @param {object} colorObject - A DTCG object containing colors with themes in Cobalt's legacy mode format.
-			 * @param {string} mode - The mode key whose value should be used.
-			 */
-			const processThemes = (colorObject, mode) => {
-				for (const value of Object.values(colorObject)) {
-					if (typeof value === 'object') {
-						if (value?.$type === 'color' && typeof value.$value === 'string') {
-							value.$value = value.$extensions.mode[mode];
-						} else {
-							processThemes(value, mode);
-						}
+		const { palette, aliases, colors, borders, space, typography } = synced;
+
+		/**
+		 * Recursively replace theme colors with value from Cobalt's legacy mode format.
+		 * @see https://cobalt-ui.pages.dev/guides/modes#with-modes
+		 * @param {object} colorObject - A DTCG object containing colors with themes in Cobalt's legacy mode format.
+		 * @param {string} mode - The mode key whose value should be used.
+		 */
+		const processThemes = (colorObject, mode) => {
+			for (const value of Object.values(colorObject)) {
+				if (typeof value === 'object') {
+					if (value?.$type === 'color' && typeof value.$value === 'string') {
+						value.$value = value.$extensions.mode[mode];
+					} else {
+						processThemes(value, mode);
 					}
 				}
-			};
+			}
+		};
 
-			/**
-			 * Recursively remove the given prefixes from value aliases.
-			 * @param {object} object - A DTCG object containing prefixed value aliases.
-			 * @param {Array} prefixes - The prefixes to remove.
-			 */
-			const removePrefixes = (object, prefixes) => {
-				for (const value of Object.values(object)) {
-					if (typeof value === 'object') {
-						if (Object.hasOwn(value, '$value') && typeof value.$value === 'string') {
-							for (const prefix of prefixes) {
-								value.$value = value.$value.startsWith(`{${prefix}.`)
-									? value.$value.replace(`{${prefix}.`, '{')
-									: value.$value;
-							}
-						} else {
-							removePrefixes(value, prefixes);
+		/**
+		 * Recursively remove the given prefixes from value aliases.
+		 * @param {object} object - A DTCG object containing prefixed value aliases.
+		 * @param {Array} prefixes - The prefixes to remove.
+		 */
+		const removePrefixes = (object, prefixes) => {
+			for (const value of Object.values(object)) {
+				if (typeof value === 'object') {
+					if (Object.hasOwn(value, '$value') && typeof value.$value === 'string') {
+						for (const prefix of prefixes) {
+							value.$value = value.$value.startsWith(`{${prefix}.`)
+								? value.$value.replace(`{${prefix}.`, '{')
+								: value.$value;
 						}
+					} else {
+						removePrefixes(value, prefixes);
 					}
 				}
-			};
+			}
+		};
 
-			/**
-			 * Remove extra properties from design tokens.
-			 * @param {object} object - A DTCG object containing extraneous properties.
-			 * @param {Array} properties - The properties to remove.
-			 */
-			const removeExtraProperties = (object, properties) => {
-				for (const prop in object) {
-					if (Object.hasOwn(object, prop)) {
-						if (typeof (object[prop]) === 'string' && properties.includes(prop)) {
-							delete object[prop];
-						}
-
-						if (typeof (object[prop]) === 'object') {
-							if (properties.includes(prop)) {
-								delete object[prop];
-							} else {
-								removeExtraProperties(object[prop], properties);
-							}
-						}
-					}
-				}
-			};
-
-			/**
-			 * 1. Create the generic UIO theme which assigns UIO primitives to all the required colors.
-			 */
-			const themes = { uio: structuredClone(colors) };
-
-			delete themes.uio.color.uio;
-
-			// All the UIO themes assign UIO primitives the same way, so we can select any to remap.
-			processThemes(themes.uio, 'bw');
-
-			/**
-			 * 2. Create all of the other contrast themes.
-			 */
-			for (const theme of ['dark', 'bw', 'wb', 'by', 'yb', 'gd', 'gw', 'lgdg', 'bbr']) {
-				themes[theme] = structuredClone(colors);
-				themes[theme].color.$type = 'color';
-
-				// Remove duplicate properties from UIO theme as they're already assigned in themes.uio (see step 2).
-				if (theme !== 'dark') {
-					for (const key of Object.keys(themes[theme].color)) {
-						if (key !== 'uio') {
-							delete themes[theme].color[key];
-						}
-					}
+		/**
+		 * Remove extra properties from design tokens.
+		 * @param {object} object - A DTCG object containing extraneous properties.
+		 * @param {Array} properties - The properties to remove.
+		 */
+		const removeExtraProperties = (object, properties) => {
+			for (const prop in object) {
+				if (!Object.hasOwn(object, prop)) {
+					continue;
 				}
 
-				processThemes(themes[theme], theme);
+				if (typeof (object[prop]) === 'string' && properties.includes(prop)) {
+					delete object[prop];
+				}
+
+				if (typeof (object[prop]) === 'object') {
+					if (properties.includes(prop)) {
+						delete object[prop];
+					} else {
+						removeExtraProperties(object[prop], properties);
+					}
+				}
 			}
+		};
 
-			/**
-			 * 3. Remove all the unnecessary group prefixes.
-			 */
-			removePrefixes(aliases, ['palette']);
-			removePrefixes(colors, ['palette', 'aliases', 'colors']);
-			removePrefixes(space, ['space']);
-			removePrefixes(themes, ['palette', 'aliases', 'colors']);
-			removePrefixes(typography, ['typography', 'responsive']);
+		/**
+		 * 1. Create the generic UIO theme which assigns UIO primitives to all the required colors.
+		 */
+		const themes = { uio: structuredClone(colors) };
 
-			/**
-			 * 4. Remove extraneous properties.
-			 */
-			removeExtraProperties(typography, ['leading']);
+		delete themes.uio.color.uio;
 
-			for (const object of [palette, aliases, colors, borders, space, typography]) {
-				removeExtraProperties(object, ['$extensions', '$description']);
-			}
+		// All the UIO themes assign UIO primitives the same way, so we can select any to remap.
+		processThemes(themes.uio, 'bw');
 
-			for (const object of [palette, aliases, colors, borders]) {
-				removeExtraProperties(object, ['$type']);
-			}
+		/**
+		 * 2. Create all of the other contrast themes.
+		 */
+		for (const theme of ['dark', 'bw', 'wb', 'by', 'yb', 'gd', 'gw', 'lgdg', 'bbr']) {
+			themes[theme] = structuredClone(colors);
+			themes[theme].color.$type = 'color';
 
-			for (const theme of Object.keys(themes)) {
-				removeExtraProperties(themes[theme], ['$extensions', '$description', '$type']);
-			}
-
-			/**
-			 * 5. Standardize types to match DTCG specification.
-			 */
-			palette.$type = 'color';
-			aliases.$type = 'color';
-			colors.$type = 'color';
-			borders.border.style.$type = 'strokeStyle';
-			borders.border.width.$type = 'dimension';
-
-			for (const key of Object.keys(typography.font)) {
-				if (key !== 'weight') {
-					typography.font[key].$type = 'fontFamily';
+			// Remove duplicate properties from UIO theme as they're already assigned in themes.uio (see step 2).
+			if (theme !== 'dark') {
+				for (const key of Object.keys(themes[theme].color)) {
+					if (key !== 'uio') {
+						delete themes[theme].color[key];
+					}
 				}
 			}
 
-			typography.text.$type = 'dimension';
-			typography.heading.$type = 'dimension';
-			typography.body.$type = 'dimension';
-			typography.diagram.$type = 'dimension';
+			processThemes(themes[theme], theme);
+		}
 
-			/** 6. Write to individual token files. */
+		/**
+		 * 3. Remove all the unnecessary group prefixes.
+		 */
+		removePrefixes(aliases, ['palette']);
+		removePrefixes(colors, ['palette', 'aliases', 'colors']);
+		removePrefixes(space, ['space']);
+		removePrefixes(themes, ['palette', 'aliases', 'colors']);
+		removePrefixes(typography, ['typography', 'responsive']);
 
-			for (const [key, value] of Object.entries({
-				palette, aliases, colors, borders, space, typography,
-			})) {
-				writeFile(`./src/design-tokens/${key}.json`, JSON.stringify(value, null, 2), 'utf8', (error) => {
-					if (error) {
-						console.error('Error writing to file', error);
-					}
-				});
+		/**
+		 * 4. Remove extraneous properties.
+		 */
+		removeExtraProperties(typography, ['leading']);
+
+		for (const object of [palette, aliases, colors, borders, space, typography]) {
+			removeExtraProperties(object, ['$extensions', '$description']);
+		}
+
+		for (const object of [palette, aliases, colors, borders]) {
+			removeExtraProperties(object, ['$type']);
+		}
+
+		for (const value of Object.values(themes)) {
+			removeExtraProperties(value, ['$extensions', '$description', '$type']);
+		}
+
+		/**
+		 * 5. Standardize types to match DTCG specification.
+		 */
+		palette.$type = 'color';
+		aliases.$type = 'color';
+		colors.$type = 'color';
+		borders.border.style.$type = 'strokeStyle';
+		borders.border.width.$type = 'dimension';
+
+		for (const [key, value] of Object.entries(typography.font)) {
+			if (key !== 'weight') {
+				value.$type = 'fontFamily';
 			}
+		}
 
-			for (const [key, value] of Object.entries(themes)) {
-				writeFile(`./src/design-tokens/themes/${key}.json`, JSON.stringify(value, null, 2), 'utf8', (error) => {
-					if (error) {
-						console.error('Error writing to file', error);
-					}
-				});
-			}
+		typography.text.$type = 'dimension';
+		typography.heading.$type = 'dimension';
+		typography.body.$type = 'dimension';
+		typography.diagram.$type = 'dimension';
+
+		/**
+		6. Write to individual token files.
+		 */
+
+		for (const [key, value] of Object.entries({
+			palette, aliases, colors, borders, space, typography,
+		})) {
+			writeFile(`./src/design-tokens/${key}.json`, JSON.stringify(value, null, 2), 'utf8', (error) => {
+				if (error) {
+					console.error('Error writing to file', error);
+				}
+			});
+		}
+
+		for (const [key, value] of Object.entries(themes)) {
+			writeFile(`./src/design-tokens/themes/${key}.json`, JSON.stringify(value, null, 2), 'utf8', (error) => {
+				if (error) {
+					console.error('Error writing to file', error);
+				}
+			});
 		}
 	});
 
