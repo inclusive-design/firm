037bd18793331b074608c3eb287647c65d2c3237
Ned Zimmerman <nzimmerman@ocadu.ca>
2026-09-09 10:27:49 -0300

    chore(deps): switch to @inclusive-design/components

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-unused-vars -- We are registering the incd-nav custom element by importing it. */
 
 import 'virtual:sugarcube.css';
-import { Nav } from '@inclusive-design/nav';
+import { Nav } from '@inclusive-design/components';
